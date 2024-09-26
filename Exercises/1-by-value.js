7b337607eeee0455988289904a09a1104c84b072
Fererra <126345645+Fererra@users.noreply.github.com>
2024-09-26 11:59:39 +0300

    implementation of two functions

--- Exercises/1-by-value.js
+++ Exercises/1-by-value.js
@@ -1,1 +1,17 @@
+// First implementation
+const inc1 = (a) => ++a;
 
+const a = 5;
+const b = inc1(a);
+
+console.dir({ a, b });
+
+// Second implementation
+const inc2 = (obj) => {
+  if (typeof obj == "object") obj.n++;
+};
+
+const obj = { n: 5 };
+inc2(obj);
+
+console.dir(obj);
